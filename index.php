332b5b5d7c5ac164afbf4944107e02a0af51ef94
Liam Hockley <xdega121@gmail.com>
2020-03-25 10:41:42 -0500

    Pre-tooling theme setup

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fresh</title>
+    <link rel="stylesheet" href="<? bloginfo("stylesheet_url") ?>">
+</head>
+<body>
+    <h1>Welcome to Fresh</h1>
+</body>
+</html>
